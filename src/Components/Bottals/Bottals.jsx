0891230fd5b5cee059b94589526237b5fb627d46
raj ali sheikhe <nahidujjamanraj@gmail.comm>
2024-01-19 08:05:51 +0600

    add my bottals number section

--- src/Components/Bottals/Bottals.jsx
+++ src/Components/Bottals/Bottals.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
+
 const Bottals = () => {
+    const [bottals, setBottals] = useState([]);
+    useEffect(() => {
+        fetch('Bottale.json')
+            .then(res => res.json())
+            .then(data => setBottals(data))
+        
+    },[])
     return (
         <div>
-            
+            <h1>The number of my Bottales:{ bottals.length}</h1>
         </div>
     );
 };
