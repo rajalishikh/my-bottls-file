bdb0aaf700daaa5f8ec6d0c2d45b9f3892b295e7
raj ali sheikhe <nahidujjamanraj@gmail.comm>
2024-01-24 10:02:45 +0600

    add my find the data section

--- src/Utility/getstorecard.js
+++ src/Utility/getstorecard.js
@@ -1,24 +1,9 @@
-// find the card 
+// find the data 
 const localStorage = () => {
-    const getCard = localStorage.getItem('card');
-    if (getCard) {
-        return JSON.parse(getCard)
+    const findTheData = localStorage.getItem('card');
+    if (findTheData) {
+      return JSON.parse(findTheData);
     }
-    return []
+    return[]
 }
-// convert the data 
-const cardConvert = card => {
-    const convertString = JSON.stringify(card);
-    localStorage.setItem('card', convertString);
-}
-
-
 // add the data 
-const card = id => {
-    const addData = localStorage();
-    addData.push(id)
-    // save the data
-    cardConvert(addData);
-
-}
-export { card };
