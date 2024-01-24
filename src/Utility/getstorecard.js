13c41c9b37a44d8693487d3f2bbe30bbf0e5d35c
raj ali sheikhe <nahidujjamanraj@gmail.comm>
2024-01-24 09:41:59 +0600

    add my find the card section

--- src/Utility/getstorecard.js
+++ src/Utility/getstorecard.js
@@ -1,27 +1,8 @@
-const getItem = () => {
-    const storeCardString = localStorage.getItem('card');
-    if (storeCardString) {
-        return JSON.parse(storeCardString)
+// find the card 
+const localStorage = () => {
+    const getCard = localStorage.getItem('card');
+    if (getCard) {
+        return JSON.parse(getCard)
     }
-    return [];
-
-
-
-
+    return []
 }
-// convert the data into strigfy 
-const saveCardTols = card => {
-    const cardStringField = JSON.stringify(card);
-    localStorage.setItem('card',cardStringField)
-}
-// add the data 
-const addLS = id => {
-    const card = getItem();
-    card.push(id);
-    // save the data 
-    saveCardTols(card);
-
-
-
-}
-export { addLS };
